9cd3b3d5e322d4063c66895525617310f653664a
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-09-04 15:53:09 -0400

    Completed game logic allowed for x and o to be placed

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-
-
-
 const circle = document.createElement("img");
 const cross = document.createElement("img");
 circle.src = "./assets/circle.png";
 cross.src = "./assets/cross.png";
 
+
 // Create Cells
-for (let i = 0; i< 9; i++) {
 
-}
+const cellOne = document.getElementById("cellOne");
+const cellTwo = document.getElementById("cellTwo");
+const cellThree = document.getElementById("cellThree");
+const cellFour = document.getElementById("cellFour");
+const cellFive = document.getElementById("cellFive");
+const cellSix = document.getElementById("cellSix");
+const cellSeven = document.getElementById("cellSeven");
+const cellEight = document.getElementById("cellEight");
+const cellNine = document.getElementById("cellNine");
 
+const cells = [cellOne, cellTwo, cellThree, cellFour, cellFive, cellSix, cellSeven, cellEight, cellNine]
 
-
-const DisplayController = {
-    updateCell(){
-
-    },
-}
 
 
 function game() {
@@ -25,8 +25,16 @@ function game() {
     const columns = 3;
     const board = [];
     let key = "X"
+    let turns = 0
+
+
     
-    for (let i = 0; i< rows; i++){
+
+    const getKey = function () {
+        return key
+    }
+    
+    for (let i = 0; i<rows; i++){
         board[i] = []
         for (let j = 0; j < columns; j++){
             board[i][j] = "*";
@@ -35,17 +43,20 @@ function game() {
 
     const togglekey = () => {
         key =="X" ? key = "O" : key = "X";
+        
     }
 
     const winGame =  function () {
         key == "X" ? console.log("Player 1 has won the game!") : console.log("Player 2 has one the game");
+        DisplayController.endGame();
     }
 
     const tieGame = function () {
         console.log("TIE GAME")
+        DisplayController.endGame();
     }
 
-    
+   
      
     const checkWin = function () {
         const transpose = function (grid) {
@@ -69,7 +80,7 @@ function game() {
             return false;
         } 
         const checkColWin = function(grid) {
-            const array = transpose(board);
+            const array = transpose(grid);
             return checkRowWin(array)
         }
 
@@ -90,35 +101,38 @@ function game() {
          
     }
 
-    const userChoice = function () {
-        const userRow= prompt("row");
-        const userColumn = prompt("column")
+    const userChoice = function (row,column) {
+        const userRow= row
+        const userColumn = column
         if(board[userRow][userColumn] != "*"){
             alert("USED")
+            return false;
         }
         else {
             board[userRow][userColumn] = key;
             console.log(board)
+            console.log(key)
+            DisplayController.updateScreen(userRow,userColumn)
+            turns++;
         }
-        
-        
-    }
 
-    const playGame = function ( count = 0) {
-        if (count >= 8) {
-            tieGame()
+        if(checkWin()) {
+            return;
         }
-        userChoice()
-        if(checkWin()){
-            return
+        else if (turns >= 9){
+            return tieGame()
         }
         key =="X" ? console.log("Player 2 turn") : console.log("Player 1 turn");
         togglekey();
-        count ++;
-        playGame(count);
+        console.log(board)
+        return true
+        
+    
     }
 
-    return {playGame}
+    
+
+    return {userChoice, getKey}
         
         
     
@@ -127,11 +141,98 @@ function game() {
 
 const myGame = game();
 
+const DisplayController = {
+    updateCell(cell){
+        switch(cell.id) {
+            case "cellOne":
+                return myGame.userChoice(0,0);
+                
+            case "cellTwo":
+                return myGame.userChoice(0,1);
+                
+            case "cellThree":
+                return myGame.userChoice(0,2);
+                
+            case "cellFour":
+                return myGame.userChoice(1,0);
+               
+            case "cellFive":
+                return myGame.userChoice(1,1);
+               
+            case "cellSix":
+                return myGame.userChoice(1,2);
+               
+            case "cellSeven":
+                return myGame.userChoice(2,0);
+               
+            case "cellEight":
+                return myGame.userChoice(2,1);
+               
+            case "cellNine":
+                return myGame.userChoice(2,2);
+               
+        }
 
 
+    },
+    endGame(){
+        for (const cell of cells) {
+            cell.removeEventListener("click", handleClick)
+        }
+    },
+    updateScreen(row,col){
+        if (row == 0) {
+            switch (col) {
+                case 0:
+                    myGame.getKey() == "X" ? cellOne.appendChild(cross.cloneNode(false)) : cellOne.appendChild(circle.cloneNode(false))
+                    break;
+                case 1:
+                    myGame.getKey() == "X" ? cellTwo.appendChild(cross.cloneNode(false)) : cellTwo.appendChild(circle.cloneNode(false))
+                    break
+                case 2:
+                    myGame.getKey() == "X" ? cellThree.appendChild(cross.cloneNode(false)) : cellThree.appendChild(circle.cloneNode(false))
+                    break;
+            }
+
+        }
+        else if (row == 1){
+            switch (col) {
+                case 0:
+                    myGame.getKey() == "X" ? cellFour.appendChild(cross.cloneNode(false)) : cellFour.appendChild(circle.cloneNode(false))
+                    break;
+                case 1:
+                    myGame.getKey() == "X" ? cellFive.appendChild(cross.cloneNode(false)) : cellFive.appendChild(circle.cloneNode(false))
+                    break;
+                case 2:
+                    myGame.getKey() == "X" ? cellSix.appendChild(cross.cloneNode(false)) : cellSix.appendChild(circle.cloneNode(false))
+                    break;  
+            }
+
+        }
+        else {
+            switch (col) {
+                case 0:
+                    myGame.getKey() == "X" ? cellSeven.appendChild(cross.cloneNode(false)) : cellSeven.appendChild(circle.cloneNode(false))
+                    break;
+                case 1:
+                    myGame.getKey() == "X" ? cellEight.appendChild(cross.cloneNode(false)) : cellEight.appendChild(circle.cloneNode(false))
+                    break;
+                case 2:
+                    myGame.getKey() == "X" ? cellNine.appendChild(cross.cloneNode(false)) : cellNine.appendChild(circle.cloneNode(false))
+                    break;    
+            }
+
+        }
+    }
+}
 
 
+for (const cell of cells) {
+    cell.addEventListener("click", handleClick)
+}
 
-
+function handleClick() {
+    DisplayController.updateCell(event.currentTarget)
+}
 
 
